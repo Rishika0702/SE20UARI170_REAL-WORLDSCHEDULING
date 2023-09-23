e7d1b0127a2b2ec1d4287294a8895c0b1037ca8c
Rishika <73593792+Rishika0702@users.noreply.github.com>
2023-09-24 00:39:40 +0530

    SE20UARI170_SHORTESTJOBFIRST.py

--- SE20UARI170_SHORTESTJOBFIRST.py
+++ SE20UARI170_SHORTESTJOBFIRST.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Process:
-    def __init__(self, pid, arrival_time, burst_time_str):
+    def __init__(self, pid, arrival_time_str, burst_time_str):
         self.pid = pid
-        self.arrival_time = arrival_time
+        self.arrival_time = self.time_to_minutes(arrival_time_str)
         self.burst_time_str = burst_time_str
         self.start_time = 0
         self.completion_time = 0
@@ -11,32 +11,32 @@ class Process:
         self.waiting_time = 0
         self.response_time = 0
 
+    def time_to_minutes(self, time_str):
+        time_parts = time_str.split(":")
+        hours = int(time_parts[0])
+        minutes = int(time_parts[1])
+        return hours * 60 + minutes
+
     def convert_burst_time(self):
-        # Parse the burst time string and convert it to minutes
         parts = self.burst_time_str.split()
         minutes = int(parts[0])
         if parts[1].lower().startswith("hour"):
             minutes *= 60
         return minutes
 
-def time_to_minutes(time_str):
-    time_obj = datetime.strptime(time_str, "%H:%M")
-    return time_obj.hour * 60 + time_obj.minute
-
 def main():
     processes_data = [
         ("A", "00:00", "30 minutes"),
         ("B", "00:10", "20 minutes"),
         ("C", "00:15", "40 minutes"),
-        ("D", "00:20", "15 mins")
+        ("D", "00:20", "15 minstues")
     ]
 
     processes = []
 
     for i, data in enumerate(processes_data):
         pid, arrival_time_str, burst_time_str = data
-        arrival_time = time_to_minutes(arrival_time_str)
-        process = Process(pid, arrival_time, burst_time_str)
+        process = Process(pid, arrival_time_str, burst_time_str)
         processes.append(process)
 
     processes.sort(key=lambda x: x.arrival_time)
@@ -88,7 +88,8 @@ def main():
     cpu_utilization = ((current_time - total_idle_time) / current_time) * 100
     throughput = len(processes) / current_time
 
-    print("\n#P\tAT\tBT\tST\tCT\tTAT\tWT\tRT\n")
+    print("\nP\tAT\tBT\t\tST\tCT\tTAT\tWT\tRT\n")
+
     for process in processes:
         print(f"{process.pid}\t{process.arrival_time}\t{process.burst_time_str}\t{process.start_time}\t{process.completion_time}\t{process.turnaround_time}\t{process.waiting_time}\t{process.response_time}\n")
 
@@ -97,7 +98,6 @@ def main():
     print(f"Average Response Time = {avg_response_time:.2f}")
     print(f"CPU Utilization = {cpu_utilization:.2f}%")
     print(f"Throughput = {throughput:.2f} process/unit time")
-
 
 if __name__ == "__main__":
     main()
